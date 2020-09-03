361ead6332fb8da2a6a1988a545b83d4b155d514
ejorgeproject <eliascjorge.e@gmail.com>
2020-09-03 12:32:46 +0000

    adicionado classificador de arvore de decisao

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print('hello word')
+from sklearn import tree
+
+X = [[150, 0], [170, 0], [140, 1], [130, 1]]
+
+y = [0, 0, 1, 1]
+
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(X, y)
+
+print(clf.predict([[160, 0]]))
